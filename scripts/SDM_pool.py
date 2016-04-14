35753f82b9f7bf617458f7ab9641bca802859ef9
g3nti <davide.genti@gmail.com>
2016-04-14 14:02:51 +0200

    pool loop

--- scripts/SDM_pool.py
+++ scripts/SDM_pool.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python
 import settings
-import os
+import subprocess,time
 
 
 def main(file):
     i=1
-    for i in sdm_device:
-        sdm_cmd="sdm120c -a %s -b %s -z 10 -i -p -v -c -f -g -P N -q %s" % (sdm_device,sdm_baudRate,sdm_usbSerial)
-        output=os.system(sdm_cmd)
-        e.write(output)
+    while i <= settings.sdm_device:
+        sdm_cmd="sdm120c -a %s -b %s -z 10 -i -p -v -c -f -g -P N -q -w2 %s" % (settings.sdm_device,settings.sdm_baudRate,settings.sdm_usbSerial)
+        proc = subprocess.Popen(sdm_cmd, stdout=subprocess.PIPE, shell=True)
+        (out, err) = proc.communicate()
+        # print sdm_cmd;
+        file.write(out)
+        
+        i=i+1
     file.close()
 
 if __name__ == '__main__':
     while True:
-        file = open(sdm_poolerLogFullPath, "w")
+        file = open(settings.sdm_poolerLogFullPath, "w")
         main(file)
-        time.sleep(sdm_poolRate)
+        time.sleep(settings.sdm_poolRate) 
--- scripts/settings.py
+++ scripts/settings.py
@@ -7,7 +7,9 @@ ramDiskPath='/run/shm/' #path di salvataggio dei log del pooler, usare il ramdis
 
 #IMPOSTAZIONI SDM120
 sdm_poolRate=5 #secondi di intervallo tra le letture
+
 sdm_device=1 #numero di device SDM120 da leggere
 sdm_baudRate=2400 # baud rate SDM120
+
 sdm_usbSerial='/dev/ttyUSB0' #path seriale
-sdm_poolerLogFullPath='%s/sdmPool.log' % (sdm_usbSerial)
+sdm_poolerLogFullPath='%s/sdmPool.log' % (ramDiskPath)
