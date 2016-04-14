f5d6550b212ac39d577a33a89d4146c8492d4250
g3nti <davide.genti@gmail.com>
2016-04-14 16:55:52 +0200

    update

--- scripts/SDM_pool.py
+++ scripts/SDM_pool.py
@@ -5,18 +5,27 @@ import subprocess,time
 
 def main(file):
     i=1
-    while i <= settings.sdm_device:
+    while i <= settings.sdm_device: #per ogni sdm dichiarato in settigns
         sdm_cmd="sdm120c -a %s -b %s -z 10 -i -p -v -c -f -g -P N -q -w2 %s" % (settings.sdm_device,settings.sdm_baudRate,settings.sdm_usbSerial)
         proc = subprocess.Popen(sdm_cmd, stdout=subprocess.PIPE, shell=True)
-        (out, err) = proc.communicate()
-        # print sdm_cmd;
-        file.write(out)
+        (out, err) = proc.communicate() #leggo i valori 
+        out=out.split( ) #splitto per spazio
+        
+        new_out=''
+        
+        
+        #brigo l'output per creare la sintassi che mi serve
+        for index,val in enumerate(out): 
+            if val != "OK": #rinuovo la stringa OK al fondo
+                if ("%sx" % val) != "x":
+                    new_out='%s%s(%s%s)\n' % (new_out,settings.sdm_device,val,settings.sdm_label[index])
+        file.write(new_out)
         
         i=i+1
     file.close()
 
 if __name__ == '__main__':
     while True:
-        file = open(settings.sdm_poolerLogFullPath, "w")
+        file = open(settings.sdm_poolerLogFullPath, "w") #apro in scrittura il file, s enon c'è lo creo
         main(file)
-        time.sleep(settings.sdm_poolRate) 
+        time.sleep(settings.sdm_poolRate) #delay per la lettura successiva
--- scripts/settings.py
+++ scripts/settings.py
@@ -6,10 +6,15 @@ ramDiskPath='/run/shm/' #path di salvataggio dei log del pooler, usare il ramdis
 
 
 #IMPOSTAZIONI SDM120
+sdm_logs_extension='.log' #seghe
+
 sdm_poolRate=5 #secondi di intervallo tra le letture
+sdm_parserRate=5 * 60 #secondi di intervallo per l'aggiornamento dello storico
+
+sdm_label = ["*V","*A","*W","*F","*Hz","*Wh"]
 
 sdm_device=1 #numero di device SDM120 da leggere
 sdm_baudRate=2400 # baud rate SDM120
 
 sdm_usbSerial='/dev/ttyUSB0' #path seriale
-sdm_poolerLogFullPath='%s/sdmPool.log' % (ramDiskPath)
+sdm_poolerLogFullPath='%s/sdmPool%s' % (ramDiskPath,sdm_logs_extension)
